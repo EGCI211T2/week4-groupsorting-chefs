2b40c4d15e45340b13fc666af4e252dcdcb69e78
thukhalwin756 <thukha.lwi@student.mahidol.edu>
2026-02-03 17:02:26 +0000

    3 sorts functions

--- sorting.h
+++ sorting.h
@@ -1,6 +1,6 @@
-#ifndefine SORTING_H
+#ifndef SORTING_H
 #define SORTING_H
-
+#include <iostream>
 #include <iomanip>
 /* function prototype*/
 
@@ -19,16 +19,34 @@ void display(int a[],int n){
 void selectionSort(int data[], int length) 
 { 
 	int i, j, m, mi; 
+  for (i = 0; i < length - 1; i++){
+  mi = i;
+    for (j = i+1; j < length; j++){
+      if (data[j] < data[mi]) { mi = j; }
+}
+m = data[i];
+data[i] = data[mi];
+data[mi] = m;
+}
 	
      display(data,length);
      
-	
 } 
 
 
 
-void insertion(int a[],int n){
-
+void insertion(int a[],int length){
+  int i, j, value;
+    for(i = 1; i < length; i++)
+{
+  value = a[i];
+  for (j = i - 1; j >= 0 && a[j] > value; j--)
+{
+  a[j + 1] = a[j];
+  display(a,i);
+}
+  a[j + 1] = value;
+  display(a,i);
 
 }
 
@@ -43,14 +61,16 @@ void swap(int &a,int &b){
 void bubbleSort(int a[],int n){
 
 int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<n;j++){
 
+    for (i = 0; i < n-1 ; i++ ) {
+        for (j = 0; j < n - i - 1 ; j++) 
+	   { 
+        if(a[j]>a[j+1]) 
+        swap(a[j],a[j+1]);
+    }
+    }
+display(a,N);
 
-	
-  display(a,n);
- }
   
 }
 #endif
